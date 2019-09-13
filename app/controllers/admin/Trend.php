1399c2b00ba4bdf0b7a2c7ec2b1ea5b9316332ee
Ferdous Anam <ferdous.anam@gmail.com>
2019-09-13 22:40:19 +0600

    Sales Trend Feature Added

--- app/controllers/admin/Trend.php
+++ app/controllers/admin/Trend.php
@@ -225,4 +225,159 @@ ON {$this->db->dbprefix('purchases')}.id = {$this->db->dbprefix('purchase_items'
         }
     }
 
+    public function sales() {
+        $this->sma->checkPermissions('sales');
+        $this->data['error'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('error');
+        $this->data['users'] = $this->reports_model->getStaff();
+        $this->data['warehouses'] = $this->site->getAllWarehouses();
+        $this->data['billers'] = $this->site->getAllCompanies('biller');
+        $bc = array(array('link' => base_url(), 'page' => lang('home')), array('link' => admin_url('reports'), 'page' => lang('reports')), array('link' => '#', 'page' => lang('Trend_Sales_Report')));
+        $meta = array('page_title' => lang('Trend_Sales_Report'), 'bc' => $bc);
+
+        $start_date = $this->input->post('start_date') ? $this->input->post('start_date') : NULL;
+        $end_date = $this->input->post('end_date') ? $this->input->post('end_date') : NULL;
+
+        $getDate = $this->start_end_date($start_date, $end_date);
+        $start_date = $getDate['start_date'];
+        $end_date = $getDate['end_date'];
+
+        $this->data['months'] = $this->monthAndYearStartAndEnd($start_date, $end_date);
+        $this->page_construct('trend/sales', $meta, $this->data);
+    }
+
+    public function getSalesReport($pdf = NULL, $xls = NULL) {
+        $this->sma->checkPermissions('sales', TRUE);
+        $product = $this->input->get('product') ? $this->input->get('product') : NULL;
+        $customer = $this->input->get('customer') ? $this->input->get('customer') : NULL;
+        $start_date = $this->input->get('start_date') ? $this->input->get('start_date') : NULL;
+        $end_date = $this->input->get('end_date') ? $this->input->get('end_date') : NULL;
+
+        $getDate = $this->start_end_date($start_date, $end_date);
+        $start_date = $getDate['start_date'];
+        $end_date = $getDate['end_date'];
+
+        $times = $this->monthAndYearStartAndEnd($start_date, $end_date);
+        $select_months = "";
+        $month_year = "";
+        foreach ($times as $time) {
+            $select_months .= ", ( SUM(IF(`month` = '" . $time['month'] . "' AND `year` = '" . $time['year'] . "', QTY, 0)) ) AS " . $time['month'] . "_" . $time['year'];
+            $month_year .= "," . $time['month'] . "_" . $time['year'] . " ";
+        }
+
+        if (!$this->Owner && !$this->Admin && !$this->session->userdata('view_right')) {
+            $user = $this->session->userdata('user_id');
+        }
+
+        $si = "( SELECT customer_id, product_id, product_code, product_name, {$this->db->dbprefix('sale_items')}.sale_id AS sale_id, {$this->db->dbprefix('sales')}.`date`, YEAR({$this->db->dbprefix('sales')}.`date`) AS `year`, MONTHNAME({$this->db->dbprefix('sales')}.`date`) AS `month`, SUM(unit_quantity) AS QTY
+FROM {$this->db->dbprefix('sales')}
+JOIN {$this->db->dbprefix('sale_items')}
+ON {$this->db->dbprefix('sales')}.id = {$this->db->dbprefix('sale_items')}.sale_id";
+
+        if ($start_date || $customer) {
+            $si .= " WHERE ";
+        }
+        if ($start_date) {
+            $si .= "{$this->db->dbprefix('sales')}.date BETWEEN '$start_date' AND '$end_date'";
+        }
+        if ($start_date && $customer) {
+            $si .= " AND ";
+        }
+        if ($customer) {
+            $si .= "customer_id = $customer";
+        }
+        $si .= " GROUP BY YEAR({$this->db->dbprefix('sales')}.`date`), MONTH({$this->db->dbprefix('sales')}.`date`), sale_id, product_id ) si";
+
+        $transposed = " (SELECT * $select_months FROM $si";
+        $transposed .= " GROUP BY product_id) tr";
+
+        if ($pdf || $xls) {
+            $this->db
+                ->select("product_id, product_code, product_name AS product_name $month_year", FALSE)
+                ->from('sales')
+                ->join($transposed, 'tr.sale_id=sales.id', 'right')
+                ->join('warehouses', 'warehouses.id=sales.warehouse_id', 'left')
+                ->group_by('product_id');
+
+            if ($product) {
+                $this->db->where('tr.product_id', $product);
+            }
+
+            $q = $this->db->get();
+            if ($q->num_rows() > 0) {
+                foreach (($q->result()) as $row) {
+                    $data[] = $row;
+                }
+            } else {
+                $data = NULL;
+            }
+
+            if (!empty($data)) {
+                $char = chr(833); // chr(833) is A
+                $this->load->library('excel');
+                $this->excel->setActiveSheetIndex(0);
+                $this->excel->getActiveSheet()->setTitle(lang('Trend_Sales_Report'));
+                if ($customer){
+                    $this->excel->getActiveSheet()->SetCellValue('B1', lang('Customer_Name'));
+                    $this->excel->getActiveSheet()->SetCellValue('C1', $this->trend_model->getCustomerById($customer)->name);
+                }
+                $this->excel->getActiveSheet()->SetCellValue('E1', lang('From'));
+                $this->excel->getActiveSheet()->SetCellValue('F1', $start_date);
+                $this->excel->getActiveSheet()->SetCellValue('G1', lang('To'));
+                $this->excel->getActiveSheet()->SetCellValue('H1', $end_date);
+
+                $first_data_row = 3;
+                $this->excel->getActiveSheet()->SetCellValue($char++ . $first_data_row, lang('SL.'));
+                $this->excel->getActiveSheet()->SetCellValue($char++ . $first_data_row, lang('Product_Code'));
+                $this->excel->getActiveSheet()->SetCellValue($char++ . $first_data_row, lang('Product_Name'));
+                foreach ($times as $time) {
+                    $this->excel->getActiveSheet()->SetCellValue($char++ . $first_data_row, $time['month'] . "_" . $time['year']);
+                }
+
+                $row = $first_data_row + 1;
+                $row_number = 1;
+                foreach ($data as $data_row) {
+                    $char = chr(833); // chr(833) is A
+                    $this->excel->getActiveSheet()->SetCellValue($char++ . $row, $row_number++);
+                    $this->excel->getActiveSheet()->SetCellValue($char++ . $row, $data_row->product_code);
+                    $this->excel->getActiveSheet()->SetCellValue($char++ . $row, $data_row->product_name);
+                    foreach ($times as $time) {
+                        $month_name = $time['month'] . "_" . $time['year'];
+                        $this->excel->getActiveSheet()->SetCellValue($char++ . $row, $data_row->$month_name);
+                    }
+                    $row++;
+                }
+
+                $char = chr(833); // chr(833) is A
+                $this->excel->getActiveSheet()->getColumnDimension($char++)->setWidth(10);
+                $this->excel->getActiveSheet()->getColumnDimension($char++)->setWidth(10);
+                $this->excel->getActiveSheet()->getColumnDimension($char++)->setWidth(50);
+                foreach ($times as $time) {
+                    $this->excel->getActiveSheet()->getColumnDimension($char++)->setWidth(15);
+                }
+                $this->excel->getDefaultStyle()->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+                $this->excel->getActiveSheet()->getStyle('B1:B' . $row)->getAlignment()->setWrapText(true);
+                $filename = 'Trend_Sales_Report';
+                $this->load->helper('excel');
+                create_excel($this->excel, $filename);
+            }
+            $this->session->set_flashdata('error', lang('nothing_found'));
+            redirect($_SERVER["HTTP_REFERER"]);
+
+        } else {
+            $this->load->library('datatables');
+            $this->datatables
+                ->select("product_code, product_name AS product_name $month_year", FALSE)
+                ->from('sales')
+                ->join($transposed, 'tr.sale_id=sales.id', 'right')
+                ->join('warehouses', 'warehouses.id=sales.warehouse_id', 'left')
+                ->group_by('product_id');
+
+            if ($product) {
+                $this->datatables->where('tr.product_id', $product, FALSE);
+            }
+
+            echo $this->datatables->generate();
+        }
+    }
+
 }
---
+++ themes/default/admin/views/trend/sales.php
@@ -0,0 +1,222 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<?php
+
+$v = "";
+if ($this->input->post('product')) {
+    $v .= "&product=" . $this->input->post('product');
+}
+if ($this->input->post('customer')) {
+    $v .= "&customer=" . $this->input->post('customer');
+}
+if ($this->input->post('start_date')) {
+    $v .= "&start_date=" . $this->input->post('start_date');
+}
+if ($this->input->post('end_date')) {
+    $v .= "&end_date=" . $this->input->post('end_date');
+}
+//echo "<pre>";
+//print_r($months);
+//echo "</pre>";
+?>
+
+<script>
+    $(document).ready(function () {
+        var source_url = "<?= admin_url('trend/getSalesReport/?v=1' . $v) ?>";
+        var colNames = [null, null
+            <?php foreach ($months as $month){?> ,{"mRender": formatQuantity} <?php } ?>];
+        console.log(colNames);
+        console.log("Columns Count: " + colNames.length);
+        oTable = $('#TrRData').dataTable({
+            "aaSorting": [[0, "asc"]],
+            "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "<?= lang('all') ?>"]],
+            "iDisplayLength": <?= $Settings->rows_per_page ?>,
+            'bProcessing': true, 'bServerSide': true,
+            'sAjaxSource': source_url,
+            'fnServerData': function (sSource, aoData, fnCallback) {
+                aoData.push({
+                    "name": "<?= $this->security->get_csrf_token_name() ?>",
+                    "value": "<?= $this->security->get_csrf_hash() ?>"
+                });
+                $.ajax({'dataType': 'json', 'type': 'POST', 'url': sSource, 'data': aoData, 'success': fnCallback});
+            },
+            'fnRowCallback': function (nRow, aData, iDisplayIndex) {
+                nRow.id = aData[0];
+                return nRow;
+            },
+            "aoColumns": colNames
+        });
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#form').hide();
+        <?php if ($this->input->post('customer')) { ?>
+        $('#customer').val(<?= $this->input->post('customer') ?>).select2({
+            minimumInputLength: 1,
+            data: [],
+            initSelection: function (element, callback) {
+                $.ajax({
+                    type: "get", async: false,
+                    url: site.base_url + "customers/suggestions/" + $(element).val(),
+                    dataType: "json",
+                    success: function (data) {
+                        callback(data.results[0]);
+                    }
+                });
+            },
+            ajax: {
+                url: site.base_url + "customers/suggestions",
+                dataType: 'json',
+                quietMillis: 15,
+                data: function (term, page) {
+                    return {
+                        term: term,
+                        limit: 10
+                    };
+                },
+                results: function (data, page) {
+                    if (data.results != null) {
+                        return {results: data.results};
+                    } else {
+                        return {results: [{id: '', text: 'No Match Found'}]};
+                    }
+                }
+            }
+        });
+
+        $('#customer').val(<?= $this->input->post('customer') ?>);
+        <?php } ?>
+        $('.toggle_down').click(function () {
+            $("#form").slideDown();
+            return false;
+        });
+        $('.toggle_up').click(function () {
+            $("#form").slideUp();
+            return false;
+        });
+    });
+</script>
+
+<div class="box">
+    <div class="box-header">
+        <h2 class="blue"><i class="fa-fw fa fa-star"></i><?= lang('Trend_Sales_Report'); ?> <?php
+            if ($this->input->post('start_date')) {
+                echo "From " . $this->input->post('start_date') . " to " . $this->input->post('end_date');
+            }
+            ?></h2>
+
+        <div class="box-icon">
+            <ul class="btn-tasks">
+                <li class="dropdown"><a href="#" class="toggle_up tip" title="<?= lang('hide_form') ?>"><i class="icon fa fa-toggle-up"></i></a></li>
+                <li class="dropdown"><a href="#" class="toggle_down tip" title="<?= lang('show_form') ?>"><i class="icon fa fa-toggle-down"></i></a></li>
+            </ul>
+        </div>
+        <div class="box-icon">
+            <ul class="btn-tasks">
+                <li class="dropdown"><a href="#" id="xls" class="tip" title="<?= lang('download_xls') ?>"><i class="icon fa fa-file-excel-o"></i></a></li>
+                <li class="dropdown"><a href="#" id="image" class="tip" title="<?= lang('save_image') ?>"><i class="icon fa fa-file-picture-o"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="box-content">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <p class="introtext"><?= lang('customize_report'); ?></p>
+
+                <div id="form">
+
+                    <?php echo admin_form_open("trend/sales"); ?>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <?= lang("product", "suggest_product"); ?>
+                                <?php echo form_input('sproduct', (isset($_POST['sproduct']) ? $_POST['sproduct'] : ""), 'class="form-control" id="suggest_product"'); ?>
+                                <input type="hidden" name="product" value="<?= isset($_POST['product']) ? $_POST['product'] : "" ?>" id="report_product_id"/>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <label class="control-label" for="customer"><?= lang("customer"); ?></label>
+                                <?php echo form_input('customer', (isset($_POST['customer']) ? $_POST['customer'] : ""), 'class="form-control" id="customer" data-placeholder="' . $this->lang->line("select") . " " . $this->lang->line("customer") . '"'); ?>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <?= lang("start_date", "start_date"); ?>
+                                <?php echo form_input('start_date', (isset($_POST['start_date']) ? $_POST['start_date'] : ""), 'class="form-control datetime" id="start_date"'); ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <?= lang("end_date", "end_date"); ?>
+                                <?php echo form_input('end_date', (isset($_POST['end_date']) ? $_POST['end_date'] : ""), 'class="form-control datetime" id="end_date"'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div
+                                class="controls"> <?php echo form_submit('submit_report', $this->lang->line("submit"), 'class="btn btn-primary"'); ?> </div>
+                    </div>
+                    <?php echo form_close(); ?>
+
+                </div>
+                <div class="clearfix"></div>
+
+                <div class="table-responsive">
+                    <table id="TrRData"
+                           class="table table-bordered table-hover table-striped table-condensed reports-table">
+                        <thead>
+                        <tr>
+                            <th><?= lang("code"); ?></th>
+                            <th><?= lang("Product_Name"); ?></th>
+                            <?php foreach ($months as $month){ ?>
+                            <th><?= lang($month['month']); ?></th>
+                            <?php } ?>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td colspan="9" class="dataTables_empty"><?= lang('loading_data_from_server') ?></td>
+                        </tr>
+                        </tbody>
+                        <tfoot class="dtFilter">
+                        <tr class="active">
+                            <th><?= lang("code"); ?></th>
+                            <th><?= lang("Product_Name"); ?></th>
+                            <?php foreach ($months as $month){ ?>
+                            <th></th>
+                            <?php } ?>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="<?= $assets ?>js/html2canvas.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#pdf').click(function (event) {
+            event.preventDefault();
+            window.location.href = "<?=admin_url('trend/getSalesReport/pdf/?v=1'.$v)?>";
+            return false;
+        });
+        $('#xls').click(function (event) {
+            event.preventDefault();
+            window.location.href = "<?=admin_url('trend/getSalesReport/0/xls/?v=1'.$v)?>";
+            return false;
+        });
+        $('#image').click(function (event) {
+            event.preventDefault();
+            html2canvas($('.box'), {
+                onrendered: function (canvas) {
+                    openImg(canvas.toDataURL());
+                }
+            });
+            return false;
+        });
+    });
+</script>
